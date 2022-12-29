180aaf3f7d3bfaa6a8feb51401ce38106e76e5f8
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-12-29 17:03:13 +0600

    Login & Register Form Design Done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import logo from '../../assets/logo/task (1).png';
+import Google from '../../assets/logo/google.png';
+import './Login.css';
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>This is Login Page</h1>
-        </div>
-    );
+  return (
+    <div>
+            <div className="text-center">
+            <img src={logo} alt="" />{" "}
+            <span className="logo-text fs-1">
+                Task <span className="daily">Daily24</span>
+            </span>
+            </div>
+
+            <div className="form-margin container p-5 rounded-3">
+                <h1 className="text-center">Log In</h1>
+                
+                {/* ///////////////Login Form ////////////////////////*/}
+                 <div className="mt-5">
+                      <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label className="fs-3">Email address</Form.Label>
+                          <Form.Control type="email" placeholder="Enter email" required/>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                          <Form.Label className="fs-3">Password</Form.Label>
+                          <Form.Control type="password" placeholder="Password" required/>
+                        </Form.Group>
+                    
+                       <div className="text-center mt-5">
+                          <Button type="submit" className="w-50 p-3 fs-5">
+                              Sign In
+                            </Button>
+                       </div>
+
+                       <div className="text-center mt-3">
+                            <Button className="btn btn-light w-50">
+                               <img src={Google} alt="" /> &nbsp; <span className="fs-5 d-none d-lg-inline">Sign in with Google</span>
+                            </Button>
+                       </div>
+
+                       <div className="mt-5">
+                            <p className="fs-5"> Are you new here ? <Link to='/register' className="fw-bold text-decoration-none text-info">Create a new account</Link></p>
+                       </div>
+                      </Form>
+                 </div>
+            </div>
+   </div>
+  );
 };
 
 export default Login;
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import logo from '../../assets/logo/task (1).png';
+import Google from '../../assets/logo/google.png';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
         <div>
-            <h1>This is Register Page</h1>
+                <div className="text-center">
+                    <img src={logo} alt="" />{" "}
+                    <span className="logo-text fs-1">
+                        Task <span className="daily">Daily24</span>
+                    </span>
+                    </div>
+
+                    <div className="form-margin container p-5 rounded-3">
+                <h1 className="text-center">Sign up</h1>
+                
+                {/* ///////////////Registration Form ////////////////////////*/}
+                 <div className="mt-5">
+                      <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label className="fs-3">Enter Your Name</Form.Label>
+                          <Form.Control type="text" placeholder="Full Name" required/>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label className="fs-3">Email address</Form.Label>
+                          <Form.Control type="email" placeholder="Enter email" required/>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label className="fs-3">Profile Picture URL (optional)</Form.Label>
+                          <Form.Control type="text" placeholder="image URL Link"/>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                          <Form.Label className="fs-3">Password</Form.Label>
+                          <Form.Control type="password" placeholder="Password" required/>
+                        </Form.Group>
+                    
+                       <div className="text-center mt-5">
+                          <Button type="submit" className="w-50 p-3 fs-5">
+                              Sign up
+                            </Button>
+                       </div>
+
+                       <div className="text-center mt-3">
+                            <Button className="btn btn-light w-50">
+                               <img src={Google} alt="" /> &nbsp; <span className="fs-5 d-none d-lg-inline">Sign up with Google</span>
+                            </Button>
+                       </div>
+
+                       <div className="mt-5">
+                            <p className="fs-5"> Already have an account ? <Link to='/login' className="fw-bold text-decoration-none text-info">Log in</Link></p>
+                       </div>
+                      </Form>
+                 </div>
+            </div>
         </div>
     );
 };
