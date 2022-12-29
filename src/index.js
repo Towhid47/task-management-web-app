6c6f304ce8d1ab4659fdf2896e638e9a4380218f
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-12-29 07:26:38 +0600

    React Bootstrap installed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// React Bootstrap CSS import
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
